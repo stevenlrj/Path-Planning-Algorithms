815fe41af633d2c4aacd105ba20358f53152e02e
Renjie Li (Steven) <rl2928@columbia.edu>
2019-04-16 16:01:05 -0400

    Update A_star.py

--- A_star.py
+++ A_star.py
@@ -2,7 +2,6 @@ import numpy as np
 import heapq
 import math
 import matplotlib.pyplot as plt
-import timeit
 
 class PriorityQueue(object):
     """Priority queue implemented by min heap used to hold nodes in exploring queue, which could achieve extracting node 
@@ -21,16 +20,17 @@ class PriorityQueue(object):
         return len(self.queue) == 0
 
 class Search(object):
-    """Search methods for discrete motion planner
+    """Search methods for path planner
     """
-    def __init__(self, world_state, robot_pose, goal_pose, obs_dis):
+    def __init__(self, world_state, robot_pose, goal_pose, obs_list, robot_size):
         self.world_state = world_state
         self.robot_pose = robot_pose
         self.goal_pose = goal_pose
-        self.obs_dis = obs_dis
         self.x_range = len(world_state)
         self.y_range = len(world_state[0])
         
+        self.robot_size = robot_size        # Robot size
+        self.obs_list = obs_list            # Obstacles
         self.frontier = PriorityQueue()     # Exploring queue
         self.cost = {}                      # Record nodes and their costs from start pose
         self.parent = {}                    # Record visitted nodes and their parents
@@ -40,7 +40,7 @@ class Search(object):
         self.parent[robot_pose] = None
 
     def cal_heuristic(self, node):
-        # Calculate distance between node and goal_pose
+        # Calculate distance between node and goal_pose as heuristic
         return (node[0] - self.goal_pose[0])**2 + (node[1] - self.goal_pose[1])**2
     
     def get_robot_motion(self, node):
@@ -59,20 +59,18 @@ class Search(object):
         return next_step
     
     def check_collision(self, x, y):
-        # Check whether node is in collision with obstacle or stay too near to obstacle
-        n = self.obs_dis
-        xr = self.x_range
-        yr = self.y_range
-        if n == 0 and self.world_state[x][y]:
+        # Check if node get collision with obstacles
+        for obs in self.obs_list:
+            if self.obs_check(x, y, obs):
+                return True
+        return False
+    
+    def obs_check(self, x, y, obs):
+        # Check if node get collision with obstacle, take into consideration about robot size
+        if x >= obs[0][0] - self.robot_size and x < obs[0][1] + self.robot_size and y >= obs[1][0] - self.robot_size and y < obs[1][1] + self.robot_size:
             return True
         else:
-            for i in range(-n, n+1):
-                for j in range(-n, n+1):
-                    x_new = x + i
-                    y_new = y + j
-                    if x_new in range(xr) and y_new in range(yr) and self.world_state[x_new][y_new]:
-                        return True
-        return False
+            return False
     
     def generate_path(self, goal):
         # Track back to get path from robot pose to goal pose
@@ -109,51 +107,33 @@ class Search(object):
                     
         return None
 
+def generate_obstacle(xr, yr):
+    # Obstacle generation
+    obs_list = [[(0, xr), (0, 1)], 
+                [(0, xr), (yr - 1, yr)], 
+                [(0, 1), (0, yr)], 
+                [(xr - 1, xr), (0, yr)]]
+    obs_list.append([(15, 16), (0, 30)])
+    obs_list.append([(35, 36), (20, 50)])
+    return obs_list
 
-def generate_graph(x_range, y_range):
+def generate_graph(xr, yr, obs_list):
     # Generate world graph with obstacles 
-    world_state = [[0] * y_range for i in range(x_range)]
-    for i in range(y_range):
-        world_state[0][i] = 1
+    ws = [[0] * yr for i in range(xr)]
+    for obs in obs_list:
+        for i in range(obs[0][0], obs[0][1]):
+            for j in range(obs[1][0], obs[1][1]):
+                ws[i][j] = 1
+    return ws
 
-    for i in range(x_range):
-        world_state[i][0] = 1
-
-    for i in range(y_range):
-        world_state[-1][i] = 1
-
-    for i in range(x_range):
-        world_state[i][-1] = 1
-
-    for i in range(y_range // 5 * 4):
-        world_state[x_range // 10 * 4 - 1][i] = 1
-
-    for i in range(y_range // 5 , y_range):
-        world_state[x_range // 10 * 6 - 1][i] = 1
-     
-    return world_state
-
-
-def generate_robot_goal(x_range, y_range, world_state):
-    # Generate robot pose and goal pose
-    i, j = 0, 0
-    p, q = 0, 0
-    
-    while world_state[i][j] or world_state[p][q] or (i, j) == (p, q):
-        i, j = np.random.randint(x_range), np.random.randint(x_range)
-        p, q = np.random.randint(y_range), np.random.randint(y_range)
-    
-    return (i, j), (p, q)
-
-    
-def show_result(op_path, world_state, robot_pose, goal_pose):
+def show_result(op_path, world_state, robot_pose, goal_pose, obs_list):
     # Plot to show result if we found a path
     OPX = []            
     OPY = []
-    WX = []             
+    SPX = []
+    SPY = []
+    WX = []            
     WY = []
-    OX = []             
-    OY = []
     SX = robot_pose[0]  
     SY = robot_pose[1]
     GX = goal_pose[0]   
@@ -164,50 +144,50 @@ def show_result(op_path, world_state, robot_pose, goal_pose):
             OPX.append(node[0])
             OPY.append(node[1])
 
-    for i in range(len(world_state)):
-        for j in range(len(world_state[0])):
-            if world_state[i][j] == 0:
-                WX.append(i)
-                WY.append(j)
-            else:
-                OX.append(i)
-                OY.append(j)
-
     xr = len(world_state)
     yr = len(world_state[0])
-    plt.figure(figsize = (8 * (xr // yr), 8))
-    plt.plot(WX, WY, "w.", label = "World")
-    plt.plot(OX, OY, "ko", label = "Obstacle", markersize = 5)
+    plt.figure(figsize=(8*(xr // yr), 8))
     plt.plot(SX, SY, "bo", label = "Robot_Pose", markersize = 8)
     plt.plot(GX, GY, "ro", label = "Goal_Pose", markersize = 8)
     
+    for obs in obs_list:
+        OX = []
+        OY = []
+        if obs[0][1] - obs[0][0] == 1:
+            for i in range(obs[1][0], obs[1][1]):
+                OX.append(obs[0][0])
+                OY.append(i)
+        else:
+            for i in range(obs[0][0], obs[0][1]):
+                OX.append(i)
+                OY.append(obs[1][0])
+        plt.plot(OX, OY, "k", linewidth = 5)
+    
     if op_path:
         plt.plot(OPX, OPY, alpha = 0.9, label = "Optimal_Path", linewidth = 3)
-        
+    
     plt.legend(loc = 'upper center', bbox_to_anchor = (0.5, -0.05), fancybox = True, shadow = True, ncol = 6)
     plt.show()
     plt.close()
 
 def main():
     # Parameter initialization
-    xr = 50
-    yr = 50 
-    obs_dis = 4   # obs_dis is the min distance between robot and obstacle for safety consideration
-    #obs_dis = 0  # If obs_dis = 0, we will get the optimal cost path, but it will also stay as near as possbile to the obstacle
-    world_state = generate_graph(xr, yr)
-    #robot_pose, goal_pose = generate_robot_goal(xr, yr, world_state)
-    robot_pose = (8 , 8) 
-    goal_pose = (xr - 8, yr - 8)
+    x_range = 50
+    y_range = 50
+    robot_size = 3
+    obs_list = generate_obstacle(x_range, y_range)
+    world_state = generate_graph(x_range, y_range, obs_list)
+    robot_pose, goal_pose = (5, 5),  (x_range - 5, y_range - 5)
     
     # Run optimal planner
-    search = Search(world_state, robot_pose, goal_pose, obs_dis)
+    search = Search(world_state, robot_pose, goal_pose, obs_list, robot_size)
     optimal_path = search.A_star()
     if optimal_path:
         print("Optimal search succeed!")
     else:
         print("No optimal path is found!")
         
-    show_result(optimal_path, world_state, robot_pose, goal_pose)
+    show_result(optimal_path, world_state, robot_pose, goal_pose, obs_list)
 
 if __name__ == '__main__':
     main()
